8fc061e62295b4620abf3fada791966aad00a10e
zichan360.yangwentao <yangwentao@zichan360.com>
2020-07-24 16:46:45 +0800

    '添加redission依赖'

---
+++ redis_learn/src/main/java/com/valhalla/RedisLearnApplication.java
@@ -0,0 +1,16 @@
+package com.valhalla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: ywt
+ * @date: 2020-07-24 16:45:19
+ **/
+@SpringBootApplication
+public class RedisLearnApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisLearnApplication.class,args);
+    }
+}
